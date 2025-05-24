0e5d1edb3484021e8f5d59c5c9f088c4436210c4
Daniel Widjojo <danielw@nurosoft.id>
2025-05-24 17:50:37 +0700

    add merge invoice line reporting

--- padirent_ds/__manifest__.py
+++ padirent_ds/__manifest__.py
@@ -10,6 +10,7 @@
         'views/sale_view.xml',
         'views/res_partner_view.xml',
         'views/account_move_view.xml',
+        'report/report_invoice_merge.xml',
     ],
     
     'installable': True
--- padirent_ds/models/account_move.py
+++ padirent_ds/models/account_move.py
@@ -6,6 +6,38 @@ class AccountMove(models.Model):
 
     package_name = fields.Text(string="Package Name")
 
+    def get_custom_invoice_lines(self):
+        for move in self:
+            merged_lines = move.invoice_line_ids.filtered(lambda l: l.merge_product)
+            normal_lines = move.invoice_line_ids.filtered(lambda l: not l.merge_product)
+
+            result = []
+            if merged_lines:
+                merged_total = sum(merged_lines.mapped('price_subtotal'))
+                result.append({
+                    'name': move.package_name or 'Merged Product',
+                    'quantity': 1,
+                    'price_unit': merged_total,
+                    'price_subtotal': merged_total,
+                    'display_type': 'product',
+                    'is_merged': True,
+                })
+
+            for line in normal_lines:
+                result.append({
+                    'name': line.name,
+                    'quantity': line.quantity,
+                    'price_unit': line.price_unit,
+                    'price_subtotal': line.price_subtotal,
+                    'display_type': line.display_type,
+                    'discount': line.discount,
+                    'taxes': ', '.join([(tax.invoice_label or tax.name) for tax in line.tax_ids]),
+                    'product_uom_id': line.product_uom_id,
+                    'merged': False
+                })
+
+            return result
+
 
 
 class AccountMoveLine(models.Model):
