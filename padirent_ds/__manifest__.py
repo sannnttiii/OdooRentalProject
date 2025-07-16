ccd98e0cb96c60d54904322d76ef719755098d8c
Daniel Widjojo <widjojo.daniel98@gmail.com>
2025-07-16 13:22:39 +0700

    hide some sales and accounting menus

--- padirent_ds/__manifest__.py
+++ padirent_ds/__manifest__.py
@@ -4,12 +4,14 @@
     "version": "18.0.0.0",
     "category": "",
     "depends": [
-        'base','sale_management', 'purchase', 'account'
+        'base', 'sale', 'sale_management', 'purchase', 'account', 'base_accounting_kit', 'base_account_budget', 'dynamic_accounts_report',
     ],
     "data": [
+        'security/security.xml',
         'views/sale_view.xml',
         'views/res_partner_view.xml',
         'views/account_move_view.xml',
+        'views/menus.xml',
         'report/report_invoice_merge.xml',
     ],
     
